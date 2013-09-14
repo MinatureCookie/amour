d15bc6cd3fcc41303aae8680ce93276954c9009c
Stephen Cook <MinatureCookie@gmail.com>
2013-09-14 16:51:15 +0100

    Fixed path position problem with .loveignore

--- ant/GenerateIgnores.java
+++ ant/GenerateIgnores.java
@@ -8,48 +8,47 @@ import java.io.IOException;
 public class GenerateIgnores{
 
 	public static void main(String[] args){
-		String path = ".";
+		String path = args[0];
 		try{
-			printAllIgnores(path);
+			printAllIgnores(path, path.length() + 1);
 		}
 		catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 
-	private static void printAllIgnores(String path) throws IOException{
-		recurseDirectory(path);
-		outputLoveIgnore(path);
+	private static void printAllIgnores(String path, int noPrintLength) throws IOException{
+		recurseDirectory(path, noPrintLength);
+		outputLoveIgnore(path, noPrintLength);
 	}
 
-	private static void recurseDirectory(String path) throws IOException{
+	private static void recurseDirectory(String path, int noPrintLength) throws IOException{
 		File root = new File(path);
 		for(File cur: root.listFiles()){
 			if(cur.isDirectory()){
-				printAllIgnores(path + "/" + cur.getName());
+				printAllIgnores(path + "/" + cur.getName(), noPrintLength);
 			}
 		}
 	}
 
-	private static void outputLoveIgnore(String path) throws IOException{
+	private static void outputLoveIgnore(String path, int noPrintLength) throws IOException{
 		File loveignore = new File(path + "/.loveignore");
 		if(loveignore.exists()){
 			BufferedReader reader = new BufferedReader(new FileReader(loveignore));
 			String currentLine;
 			while((currentLine = reader.readLine()) != null){
-				System.out.print(printablePath(path) + currentLine + "/**, ");
+				System.out.print(printablePath(path, noPrintLength) + currentLine + "/**, ");
 			}
 		}
 	}
 
-	private static String printablePath(String path){
-		String printablePath = path + "/";
-
-		if(path.equals("") || path.equals(".")){
+	private static String printablePath(String path, int noPrintLength){
+		String printablePath;
+		if(noPrintLength > path.length()){
 			printablePath = "";
 		}
-		else if(path.length() > 2 && path.substring(0, 2).equals("./")){
-			printablePath = path.substring(2) + "/";
+		else{
+			printablePath = path.substring(noPrintLength) + "/";
 		}
 
 		return printablePath;
